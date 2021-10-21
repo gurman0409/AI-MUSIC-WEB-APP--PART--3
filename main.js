5d86bbd27eb5d5bf429cabaaf57b0f352e8cfad0
gurman0409 <78150793+gurman0409@users.noreply.github.com>
2021-10-21 13:13:40 +0530

    Add files via upload

---
+++ main.js
@@ -0,0 +1,49 @@
+song = "";
+song2 = "";
+left_wrist_y = 0;
+left_wrist_x = 0;
+right_wrist_x = 0;
+right_wrist_y = 0;
+
+function setup()
+{
+    canvas = createCanvas(450,450);
+    canvas.center();
+    video = createCapture(VIDEO);
+    video.size(450,450);
+    video.hide();
+
+    poseNet = ml5.poseNet(video, modelLoaded);
+    poseNet.on('pose', gotposes);
+}
+
+function modelLoaded() {
+    console.log('modelLoaded');
+}
+
+function gotposes(results)
+{
+  if (results.length>0)
+  {
+      console.log(results);
+      left_wrist_x = results[0].pose.leftWrist.x;
+      left_wrist_y = results[0].pose.leftWrist.y;
+      console.log("left_wrist_x = " + left_wrist_x + "left_wrist_y = " + left_wrist_y);
+
+      right_wrist_x = results[0].pose.rightWrist.x;
+      right_wrist_y = results[0].pose.rightWrist.y;
+      console.log("right_wrist_x = " + right_wrist_x + "right_wrist_y = " + right_wrist_y);
+      console.log
+  }
+}
+
+function preload()
+{
+    song1 = loadSound("music.mp3");
+    song2 = loadSound("music2.mp3");
+}
+
+function draw()
+{
+    image(video,0,0,450,450);
+}
